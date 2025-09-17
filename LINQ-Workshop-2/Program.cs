66da2a1f237b525d0593591875f22d25ed97253c
Sebastian Linero <sebastianlinero15@outlook.com>
2025-09-17 17:25:36 -0500

    Add new projects for LINQ Workshop and update Program.cs with exercises

--- LINQ-Workshop-2/Program.cs
+++ LINQ-Workshop-2/Program.cs
@@ -1,3 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+// exercise 2 
+var names = new List<string> {"Bob", "Charlie Wonka David Perceval", "David","Eve", "Alice", "Alan", "Andrew"};
+
+//  Order names alphabetically
+var orderedNames = names.OrderBy(n => n).ToList();
+Console.WriteLine("Ordered Names: " + string.Join(", ", orderedNames));
+
+// Select names in list names that start with letter 'A'
+var selectNamesWhichA = names.Where(n=> n.StartsWith("A")).ToList();
+Console.WriteLine("Names staring which A: "  + string.Join(", ", selectNamesWhichA));
+
+// Count how many names have lenght more five
+var namesHighest5 = names.Count(n => n.Length > 5);
+Console.WriteLine("Highest 5: " + string.Join(", ", namesHighest5));
+
+//Here are a couple of ways to concatenate all the names in a list into a single string separated by spaces
+
+var namesConcatenate = string.Join(" ", names);
+Console.WriteLine("Names concatenate: " + namesConcatenate);
+
+// Search and view the name highest in the list
+
+var nameHighest = names.OrderByDescending(n => n.Length).First();
+Console.WriteLine("Highest name is: " + nameHighest);
--- LINQ-Workshop/Program.cs
+++ LINQ-Workshop/Program.cs
@@ -20,34 +20,3 @@ Console.WriteLine("Count of Numbers <= 3: " + countNumbersLowerOrEqualTo3);
 // Fifth select each number multiply by 2
 var multipliedBy2 = numbers.Select (n => n * 2).ToList();
 Console.WriteLine("Numbers Multiplied by 2: " + string.Join(", ", multipliedBy2));
-
-// exercise 2 
-var names = new List<string> {"Bob", "Charlie Wonka David Perceval", "David","Eve", "Alice", "Alan", "Andrew"};
-
-//  Order names alphabetically
-var orderedNames = names.OrderBy(n => n).ToList();
-Console.WriteLine("Ordered Names: " + string.Join(", ", orderedNames));
-
-// Select names in list names that start with letter 'A'
-var selectNamesWhichA = names.Where(n=> n.StartsWith("A")).ToList();
-Console.WriteLine("Names staring which A: "  + string.Join(", ", selectNamesWhichA));
-
-// Count how many names have lenght more five
-var namesHighest5 = names.Count(n => n.Length > 5);
-Console.WriteLine("Highest 5: " + string.Join(", ", namesHighest5));
-
-//Here are a couple of ways to concatenate all the names in a list into a single string separated by spaces
-
-var namesConcatenate = string.Join(" ", names);
-Console.WriteLine("Names concatenate: " + namesConcatenate);
-
-// Search and view the name highest in the list
-
-var nameHighest = names.OrderByDescending(n => n.Length).First();
-Console.WriteLine("Highest name is: " + nameHighest);
-
-
-
- 
-// Exercise 3
-// continue exercise
